1693a735679cc4a463e1334eed87086b7d45e99d
Gabe Arnold <gabearnold237@gmail.com>
2023-03-09 11:28:24 -0700

    troubles with encoder working

--- Project_Light_Lantern/src/Project_Light_Lantern.cpp
+++ Project_Light_Lantern/src/Project_Light_Lantern.cpp
@@ -21,18 +21,23 @@ void setup();
 void loop();
 void pixelFill(int startPixel,int endPixel,int color);
 #line 14 "c:/Users/gabea/Documents/IoT/Smart-Room-controller/Project_Light_Lantern/src/Project_Light_Lantern.ino"
-int PINA=A5;
-int PINB=A4;
+int PINA=D11;
+int PINB=D12;
 const int PIXELPIN=D5;
-const int PIXELNUMBER=15;
+const int PIXELNUMBER=16;
 int pixelBrightness;
-// int maxPos=15;
+int maxPos=15;
 int startPixel=0;
 int endPixel=15;
 int color=0x008000;
+int positionB;
 int positionA;
 int pixelCount;
 const int OLED_RESET=4;
+bool buttonState;
+int buttonPin=A3;
+const int MYWEMO1=1;
+const int MYWEMO2=2;
 
 Adafruit_SSD1306 display(OLED_RESET);
 Adafruit_NeoPixel pixel(PIXELNUMBER,PIXELPIN,WS2812B);
@@ -48,8 +53,10 @@ void setup() {
   Serial.begin(9600);
   waitFor(Serial.isConnected,15000);
   display.begin (SSD1306_SWITCHCAPVCC, 0x3c);
+  display.display();
+  delay(200);
   display.clearDisplay();
-  display.drawBitmap(16,20, myBitmap,231, 105, 1);
+  display.drawBitmap(16,20, myBitmap,112, 44, 1);
   display.display();
   
   display.begin(SSD1306_SWITCHCAPVCC, 0x3c);
@@ -78,41 +85,51 @@ void setup() {
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
+   buttonState=digitalRead(buttonPin);
  positionA=myEnc.read();
- pixelBrightness=map(positionA,0,95,0,15);
+ pixelBrightness=map(positionA,0,95,0,127);
  pixel.setBrightness(pixelBrightness);
  pixelFill(startPixel,endPixel,color);
- Serial.printf("%i %i",positionA,pixelBrightness);
-//  myEnc.write(maxPos);  
+// Serial.printf("val of my encoder/n");
+ Serial.printf("%i %i\n",positionA,pixelBrightness);
+ myEnc.write(maxPos);  
     // Serial.printf("%i,%i\n",positionA,pixelBrightness);
     val=analogRead(Tswitch);
     brightness=255;
-    Serial.printf("val=%i\n",val);
+    // Serial.printf("val=%i\n",val);
     if (val<200){
      for (BULB=1; BULB<=3; BULB++){
-      setHue(BULB,false,HueGreen,0,255); 
-     Serial.printf("bulb off");
-     for (BULB=4; BULB<=6; BULB++);{
-      setHue(BULB, false, HueBlue,0,255);
-     }
+      // setHue(BULB,false,HueGreen,0,255);
+      //switchOFF(MYWEMO1);
+
+     } 
+    //  Serial.printf("bulb off");
+     for (BULB=4; BULB<=6; BULB++){
+      // setHue(BULB, false, HueBlue,0,255);
+      //switchOFF(MYWEMO2);
      }
     }
     
     else{
       for (BULB=1; BULB<=3; BULB++){
-        setHue(BULB,true,HueGreen,brightness,255);
-        Serial.printf("bulb on");
-        for (BULB=4; BULB<=6; BULB++);{
-          setHue(BULB, true, HueBlue,brightness,255);
- }
+        // setHue(BULB,true,HueGreen,brightness,255);
+        // Serial.printf("bulb on");
+        //switchON(MYWEMO1);
+      }
+        for (BULB=4; BULB<=6; BULB++){
+          // setHue(BULB, true, HueBlue,brightness,255);
+          //switchON(MYWEMO2);
+ 
 }
 }
 }
 void pixelFill(int startPixel,int endPixel,int color){
-    for (pixelCount=startPixel; pixelCount<=endPixel; pixelCount++)
+    for (pixelCount=startPixel; pixelCount<=endPixel; pixelCount++){
       pixel.setPixelColor(pixelCount,color);
       pixel.show();
-      pixel.clear();
-      // Serial.printf("encoder value%i\r",positionA);
-
 }
+      pixel.clear();
+}
+
+     
+
